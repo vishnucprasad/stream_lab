aa409bcf47a6cd01fafd04800f4483fce39725d2
Vishnu C Prasad <mail@vishnucprasad.in>
2024-09-14 08:47:13 +0530

    Configured dependency injection with get_it and injectable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:stream_lab/core/injection/injection.dart';
 import 'package:stream_lab/presentation/core/stream_lab_app.dart';
 
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   runApp(const StreamLabApp());
 }
