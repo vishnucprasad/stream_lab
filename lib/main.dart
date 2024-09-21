9ad9504f31c790facc625124d1cda2295b813553
Vishnu C Prasad <mail@vishnucprasad.in>
2024-09-21 11:13:40 +0530

    Setup Hive DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:stream_lab/core/injection/injection.dart';
 import 'package:stream_lab/presentation/core/stream_lab_app.dart';
 
 Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   await configureDependencies();
   runApp(const StreamLabApp());
 }
